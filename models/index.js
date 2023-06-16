25cf7fd2dd9ad77bcea22f60a527a8effe560845
Jessica Yun <jessicasopark@gmail.com>
2023-06-16 09:49:31 -0700

    Added database models

--- models/index.js
+++ models/index.js
@@ -1,20 +1,35 @@
-const { Model, DataTypes } = require('sequelize');
+// import models
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
 
-const sequelize = require('../config/connection.js');
+// Products belongsTo Category
+Product.belongsToMany(Category, {
+  foreignKey: 'category_id'
+});
 
-class Category extends Model {}
+// Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'SET NULL'
+});
 
-Category.init(
-  {
-    // define columns
-  },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'category',
-  }
-);
+// Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id'
+});
 
-module.exports = Category;
+// Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id'
+});
+
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
