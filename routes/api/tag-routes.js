c16bd89551dd53470ade8af2e69403ccfb961092
Jessica Yun <jessicasopark@gmail.com>
2023-06-19 19:41:48 -0700

    Added tag routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -19,7 +19,7 @@ router.get('/:id', async (req, res) => {
   // be sure to include its associated Product data
   try {
     const tagData = await Tag.findByPk(req.params.id, {
-      include: [{ model: Tag, through: Product, as: 'product-routes' }]
+      include: [{model: Product, through: ProductTag}]
     });
 
     if (!tagData) {
@@ -46,7 +46,7 @@ router.post('/', async (req, res) => {
 router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
   try {
-    const tagData = await Tag.update(req.body);
+    const tagData = await Tag.update(req.body, {where: {id:req.params.id}});
     res.status(200).json(tagData);
   } catch (err) {
     res.status(400).json(err);
